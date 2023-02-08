ed4bdca994a2edc1f1174dd2337ecfeea10ccb67
MrKirilsReinke <120989319+MrKirilsReinke@users.noreply.github.com>
2023-02-08 14:09:57 +0200

    Add files via upload

---
+++ src/constants/images.ts
@@ -0,0 +1,9 @@
+const carouselImages: string[] = [
+  'https://i.pinimg.com/564x/85/02/57/8502577156ad9ca457e092c791c5117f.jpg',
+  'https://i.pinimg.com/564x/0e/62/7c/0e627c12c05e4dd93ab122d618ea7849.jpg',
+  'https://i.pinimg.com/564x/14/f9/bd/14f9bdebc10f0503bba1e115e47b5c69.jpg',
+  'https://i.pinimg.com/564x/57/b4/7d/57b47d755314f919be8149e642bd862e.jpg',
+  'https://i.pinimg.com/564x/e1/99/b2/e199b25cd826924f861cf8762eee449c.jpg',
+];
+
+export default carouselImages;
